909170ab66cca0016014438ba2dfbc7a73cea83f
tomlowen <tom.r.lowen@googlemail.com>
2021-10-21 14:30:49 +0100

    commetns added for routes

--- models/responses.js
+++ models/responses.js
@@ -1,10 +1,10 @@
 //import 'query' that we created in db/index.js so we can use it here instead
 const { query } = require("../db/index");
 
-//function that returns all responses from the DB, as well as the associated graduate info
-
-//function that takes in a graduateuuid, and returns all responses associtaed with that graduate
-
 //function that takes in a uuid, and deletes all responses with that uuid
 
 //function that takes in a response object, and creates a new response row in the DB
+
+//function that takes in a graduateuuid, and returns all responses associtaed with that graduate
+
+//function that returns all responses from the DB, as well as the associated graduate info
--- routes/responses.js
+++ routes/responses.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
+/* GET all responses listing. */
 router.get("/", function(req, res, next) {
   res.send("This is the responses router");
 });
+
+// GET response by param
 
 module.exports = router;
