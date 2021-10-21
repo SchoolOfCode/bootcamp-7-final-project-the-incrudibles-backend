e110a47dc80f393e550a4594b6774fed2bd1b4e6
tomlowen <tom.r.lowen@googlemail.com>
2021-10-21 13:37:09 +0100

    comments added prior to writing functions

---
+++ models/graduates.js
@@ -0,0 +1,12 @@
+//import 'query' that we created in db/index.js so we can use it here instead
+const { query } = require("../db/index");
+
+//function that returns all graduates from the DB
+
+//function that takes in a uuid, and returns all graduates with that uuid
+
+//function that takes in a uuid, and deletes all graduates with that uuid
+
+//function that takes in a graduate object, and creates a new graduate row in the DB
+
+//function that takes in a graduate object and a uuid, and updates a graduate matching the uuid in the DB with that object
