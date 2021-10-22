35d1079afa4d6cc641c6b98d94b244f1486a1678
tomlowen <tom.r.lowen@googlemail.com>
2021-10-22 11:38:40 +0100

    database pool connection updated with URL

--- db/index.js
+++ db/index.js
@@ -1,13 +1,13 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  // connectionString: process.env.DATABASE_URL,
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  ssl: { rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,
+  // user: process.env.PGUSER,
+  // host: process.env.PGHOST,
+  // database: process.env.PGDATABASE,
+  // password: process.env.PGPASSWORD,
+  // port: process.env.PGPORT,
+  // ssl: { rejectUnauthorized: false },
 });
 
 module.exports = {
