40d9fc7930a35ed4461d8a5bfa0f209819bf1589
tomlowen <tom.r.lowen@googlemail.com>
2021-10-22 10:36:20 +0100

    CRUD routes finsihed for graduates

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  // user: process.env.PGUSER,
-  // host: process.env.PGHOST,
-  // database: process.env.PGDATABASE,
-  // password: process.env.PGPASSWORD,
-  // port: process.env.PGPORT,
+  // connectionString: process.env.DATABASE_URL,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
   ssl: { rejectUnauthorized: false },
 });
 
--- routes/graduates.js
+++ routes/graduates.js
@@ -1,12 +1,62 @@
 var express = require("express");
-const { getAllGraduates } = require("../models/graduates");
 var router = express.Router();
 
+const {
+  getAllGraduates,
+  getGraduateByUuid,
+  deleteGraduateByUuid,
+  postNewGraduate,
+  updateGraduate,
+} = require("../models/graduates");
+
+router.get("/:id", async (req, res) => {
+  console.log(req.params);
+  const { id } = req.params;
+  let data = await getGraduateByUuid(id);
+  res.json({
+    success: true,
+    message: "Here is the graduate with id " + id,
+    payload: data,
+  });
+});
+
 router.get("/", async (req, res) => {
+  console.log(req.params);
   const data = await getAllGraduates();
   res.json({
     success: true,
     message: "Here are all the graduates",
+    payload: data,
+  });
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = await deleteGraduateByUuid(id);
+  res.json({
+    success: true,
+    message: `Graduate ${id} has been deleted form the database`,
+    payload: data,
+  });
+});
+
+router.post("/", async (req, res) => {
+  const graduate = req.body;
+  const data = await postNewGraduate(graduate);
+  res.json({
+    success: true,
+    message: `Graduate ${graduate.graduateName} has been added to the database`,
+    payload: data,
+  });
+});
+
+router.put("/:uuid", async (req, res) => {
+  const { id } = req.params;
+  const graduate = req.body;
+  const data = await updateGraduate(graduate, id);
+  res.json({
+    success: true,
+    message: `Graduate ${graduate.graduateName} has been updated`,
     payload: data,
   });
 });
