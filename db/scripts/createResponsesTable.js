5dbdc35fdf77d22e1c337fa0b75e912451ea3ee9
tomlowen <tom.r.lowen@googlemail.com>
2021-10-22 11:05:06 +0100

    table scripts finished

--- db/scripts/createResponsesTable.js
+++ db/scripts/createResponsesTable.js
@@ -3,7 +3,7 @@ const { query } = require("../index"); //importing query
 async function createResponsesTable() {
   await query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
   await query(
-    "CREATE TABLE responses (id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), graduateUuid UUID NOT NULL REFERENCES graduates (id), isEmployed BOOLEAN, techRole BOOLEAN, currentSalary INTEGER, currentEmployer VARCHAR(255), lengthOfService VARCHAR(255), currentRole TEXT, currentTechStack TEXT [], jobSatisfaction INTEGER)"
+    "CREATE TABLE responses (id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), graduateUuid UUID NOT NULL REFERENCES graduates (id), timestamp TIMESTAMP, isEmployed BOOL, techRole BOOL, currentSalary INTEGER, currentEmployer VARCHAR(255), lengthOfService VARCHAR(20), currentRole TEXT, currentTechStack TEXT [], jobSatisfaction SMALLINT)"
   );
   console.log("responses table has been created");
   return;
