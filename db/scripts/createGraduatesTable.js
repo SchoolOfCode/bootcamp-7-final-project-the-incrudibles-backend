73120add5c92c82cf5de79a175d0dd2d1d462c63
tomlowen <tom.r.lowen@googlemail.com>
2021-10-22 09:01:39 +0100

    minor bug fix

--- db/scripts/createGraduatesTable.js
+++ db/scripts/createGraduatesTable.js
@@ -3,7 +3,7 @@ const { query } = require("../index"); //importing query
 async function createGraduatesTable() {
   await query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
   await query(
-    "CREATE TABLE graduates (id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), graduateName VARCHAR(255), graduateEmail VARCHAR(255), cohort SMALLINT)"
+    "CREATE TABLE graduates (id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), graduateName VARCHAR(255) NOT NULL, graduateEmail VARCHAR(255), cohort SMALLINT)"
   );
   console.log("graduates table has been created");
   return;
