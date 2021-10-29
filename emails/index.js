0fd6b0e8dd3414a37a5226591a0863582227bf7a
himself949 <doubleh290986@hotmail.com>
2021-10-29 11:03:08 +0100

    send grid dynamic data added

--- emails/index.js
+++ emails/index.js
@@ -1,6 +1,10 @@
-/*const graduateemail = "nasra_diini@hotmail.com"
+const sgMail = require("@sendgrid/mail");
+const API_KEY = "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";
+sgMail.setApiKey(API_KEY);
+
+const graduateemail = "himeshpatel@live.co.uk"
 const id = "9960760f-b212-49b4-b973-331d5be32da8"
-*/
+const name = "himesh"
 
 /*
 const message = {
@@ -14,17 +18,24 @@ const message = {
 };
 */
 const message = {
-  to: 'himeshpatel@live.co.uk',
+  to: graduateemail,
   from: {
-    name: "SoC",
+    name: "School Of Code",
     email: "incrudibles@hotmail.com",
   },
   templateId: 'd-f737af32cca54db481684f36144d0491',
-
+  subject: "Beyond Bootcamp",
   dynamic_template_data: {
-    example: '',
+    id: id,
+    name: name
   },
 };
 
-module.exports = message;
+sgMail
+  .send(message)
+  .then((response) => {
+    console.log(message);
+    console.log("Email has been sent!");
+  })
+  .catch((error) => console.log(error.message));
 
