565480c362f11b895b29d28179ffde79b58a0422
diinin <diininasra94@gmail.com>
2021-10-29 13:49:21 +0100

    finished off email

--- emails/index.js
+++ emails/index.js
@@ -1,7 +1,8 @@
 const usFour = require("../data/usFour");
+const html = require("../emails/index.html");
 
-const graduateemail = "himeshpatel@live.co.uk"
-const id = "abbfe044-3b3d-4921-92c2-18a1ab7601d5"
+const graduateemail = "himeshpatel@live.co.uk";
+const id = "abbfe044-3b3d-4921-92c2-18a1ab7601d5";
 
 const message = {
   to: graduateemail,
@@ -11,7 +12,9 @@ const message = {
   },
   subject: "sendGrid Test",
   text: "Hello from SendGrid",
-  html: `<a href="https://peaceful-darwin-2d2aa9.netlify.app/?graduates=${id}">Click here</a>`,
+  html: html,
 };
+
+
 
 module.exports = message;
